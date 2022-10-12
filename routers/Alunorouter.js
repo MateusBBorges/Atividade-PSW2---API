dd7d008510a52ea216cf0730f4483f5653b5b1f2
levimorais <silvalevi690@gmail.com>
2022-10-12 12:22:22 -0300

    criando rotas para criar um novo cadastro de aluno, bem como atualizar email, serie e telefone de qualquer aluno

--- routers/Alunorouter.js
+++ routers/Alunorouter.js
@@ -19,4 +19,45 @@ AlunoRouter.get('/alunoid', async (req, res) => {
     }
 });
 
+AlunoRouter.post('/aluno-cadastrar', async (req, res) => {
+    try{
+        const result = await Aluno.create(req.body);
+        res.json({result});
+    } catch (error) {
+        res.json({mensagem: 'Ocorreu um erro durante o cadastro'});
+    }
+    
+
+});
+
+AlunoRouter.put('/aluno-atualizar-email', async (req, res) => {
+    try{
+        const result = await Aluno.updateOne({email: req.body.email}, req.body);
+        res.json({result});
+    } catch (error) {
+        res.json({mensagem: 'Ocorreu um erro durante a atualização'});
+    }
+
+});
+
+AlunoRouter.put('/aluno-atualizar-telefone', async (req, res) => {
+    try{
+        const result = await Aluno.updateOne({email: req.body.telefone}, req.body);
+        res.json({result});
+    } catch (error) {
+        res.json({mensagem: 'Ocorreu um erro durante a atualização'});
+    }
+
+});
+
+AlunoRouter.put('/aluno-atualizar-serie', async (req, res) => {
+    try{
+        const result = await Aluno.updateOne({email: req.body.serie}, req.body);
+        res.json({result});
+    } catch (error) {
+        res.json({mensagem: 'Ocorreu um erro durante a atualização'});
+    }
+
+});
+
 module.exports = AlunoRouter;
